a4c3814739ef646e5261a6082ffd24b961531521
Vladislav Vaganov <VladislavVaganov@mysale.com>
2020-11-05 22:31:57 +0300

    configured client's proxy to API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const fetchData = async () => {
+  const response = await fetch('/list');
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    throw Error(data.message);
+  }
+
+  return data;
+};
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData()
+      .then((res) => setData(res))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +37,12 @@ function App() {
         >
           Learn React
         </a>
+        <b>
+          Data from API:
+          {data.map((item) => (
+            <div>{item}</div>
+          ))}
+        </b>
       </header>
     </div>
   );
