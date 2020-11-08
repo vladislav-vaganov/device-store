48b666560f7e4af27a370a10b163713edf256086
Vladislav Vaganov <VladislavVaganov@mysale.com>
2020-11-08 16:47:12 +0300

    configured docker-compose

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/api/',
     createProxyMiddleware({
       target: 'http://localhost:7337',
-      pathRewrite: { '^/api': '' },
+      pathRewrite: { '^/api/': '/' },
       changeOrigin: true,
     })
   );
